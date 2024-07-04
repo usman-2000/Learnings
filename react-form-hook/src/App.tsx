3119bc1361d350846f570a1b9b45041141152851
usman-2000 <usmanrahim2000@gmail.com>
2024-07-05 00:54:44 +0500

    resolver is used to write validation logics, it has all values of the form object.

--- react-form-hook/src/App.tsx
+++ react-form-hook/src/App.tsx
@@ -47,46 +47,15 @@ function App() {
     <div className="App">
       <h1>Registeration</h1>
       <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <input
-          {...register("username", {
-            required: "Username is required",
-            minLength: {
-              value: 3,
-              message: "The username must be greater than or equal to 3",
-            },
-          })}
-          type="text"
-          name="username"
-        />
+        <input {...register("username")} type="text" name="username" />
         {errors.username && (
           <div style={{ color: "red" }}>{errors.username.message}</div>
         )}
-        <input
-          {...register("email", {
-            required: "email is required",
-            validate: (val) => {
-              if (!val.includes("@")) {
-                return "Email must be valid and includes @";
-              }
-            },
-          })}
-          type="string"
-          name="email"
-        />
+        <input {...register("email")} type="string" name="email" />
         {errors.email && (
           <div style={{ color: "red" }}>{errors.email.message}</div>
         )}
-        <input
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 4,
-              message: "The minimum length of a password must be 4",
-            },
-          })}
-          type="password"
-          name="password"
-        />
+        <input {...register("password")} type="password" name="password" />
         {errors.password && (
           <div style={{ color: "red" }}>{errors.password.message}</div>
         )}
