aee8521baada3dac0582d7ec8094618e4354182c
usman-qsols <usmanrahim.qsols@gmail.com>
2024-07-25 01:23:44 +0500

    Word counter

--- word-counter-app/app.js
+++ word-counter-app/app.js
@@ -1,7 +1,17 @@
-function rendertext(text) {
-
+function rendertext(ele, text) {
+    return ele.innerHTML = text
 }
 
 function handleWord(text) {
+    var wordsCount = text.split(' ')
+    return wordsCount
+}
+
+function handleChar(text) {
+    var charsCount = text.length
+    return charsCount
+}
+
+function init() {
 
 }
