487f4a9448a2e8818069103037ab94a62c5fb91f
usman-2000 <usmanrahim2000@gmail.com>
2024-09-23 17:12:15 +0500

    design patterns

--- design-patterns-react/src/hooks/useCount.ts
+++ design-patterns-react/src/hooks/useCount.ts
@@ -1,5 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function useCount() {
-    const [count, setCount] = useState()
+    const [count, setCount] = useState(0)
+
+    useEffect((() => {
+        const saveCount = localStorage.getItem('count')
+        if (saveCount) {
+            setCount(Number(saveCount))
+        }
+    }), [])
+
+    function increment() {
+        return setCount(count + 1)
+    }
+
+    function deccrement() {
+        return setCount(count - 1)
+    }
+
+
 }
