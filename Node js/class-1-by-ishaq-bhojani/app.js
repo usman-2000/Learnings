5be0de6878ca68ca168d7a14e8e9d933f7935f30
usman-2000 <usmanrahim2000@gmail.com>
2024-11-30 23:00:39 +0500

    Node js

--- Node js/class-1-by-ishaq-bhojani/app.js
+++ Node js/class-1-by-ishaq-bhojani/app.js
@@ -3,11 +3,20 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         console.log("Checking")
-        res.write(`<h1>Hello Usman</h1> 
-            <input placeholder='hehe' type= 'text' />`)
+        res.setHeader('content-type', 'text/html')
+        res.write(`
+            <form action='/submit' method='POST'>
+            <h1>Hello Usman</h1> 
+            <input placeholder='hehe' type= 'text' /> <button type='submit' >Submit</button>
+            </form>
+            `)
         res.end()
 
-    } else {
+    } else if (req.url == '/submit') {
+        res.write(req.url)
+    }
+
+    else {
         res.write('404 - Not Found')
         res.end()
     }
